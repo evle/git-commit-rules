cffe714a17e294c061491f1370e95743fec1dc7f
unknown <lixiangyu1@corp.qihoo.net>
2020-05-28 16:56:41 +0800

    refactor: 修改提交检测方式

--- .cz-config.js
+++ .cz-config.js
@@ -16,15 +16,9 @@ module.exports = {
 
 
   messages: {
-    type: 'Select the type of change that you\'re committing:',
-    scope: '\nDenote the SCOPE of this change (optional):',
-    // used if allowCustomScopes is true
-    customScope: 'Denote the SCOPE of this change:',
-    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
-    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-    breaking: 'List any BREAKING CHANGES (optional):\n',
-    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
-    confirmCommit: 'Are you sure you want to proceed with the commit above?'
+    type: '选择本次提交类型:',
+    subject: '请认真书写提交内容:\n',
+    confirmCommit: '确认提交以上信息?'
   },
 
   allowBreakingChanges: ['feat', 'fix'],
